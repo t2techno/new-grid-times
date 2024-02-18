2f3412061891692a9023b06473df89d700d8b3a7
t2techno <ttuschhoff11@gmail.com>
2024-02-18 15:00:25 -0600

    Responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
-    <header>
+    <HWrapper>
       <SuperHeader>
         <Row>
           <ActionGroup>
@@ -21,29 +21,52 @@ const Header = () => {
               <Menu size={24} />
             </button>
           </ActionGroup>
+          <LaptopUpLogo>
+            <Logo />
+          </LaptopUpLogo>
           <ActionGroup>
-            <button>
+            <TabletDownButton>
               <User size={24} />
-            </button>
+            </TabletDownButton>
+            <LaptopUpSubscribe>
+              <SubscribeButton>SUBSCRIBE</SubscribeButton>
+              <p>
+                <i>Already a subscriber?</i>
+              </p>
+            </LaptopUpSubscribe>
           </ActionGroup>
         </Row>
       </SuperHeader>
-      <MainHeader>
+      <TabletDownLogo>
         <Logo />
-      </MainHeader>
-    </header>
+      </TabletDownLogo>
+    </HWrapper>
   );
 };
+
+const HWrapper = styled.header`
+  position: relative;
+  max-width: calc(1200 / 16) rem;
+`;
 
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media (${QUERIES.laptopAndUp}) {
+    background: revert;
+    color: var(--color-gray-900);
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
   display: flex;
   justify-content: space-between;
+  
+@media(${QUERIES.laptopAndUp}){
+  align-items: baseline;
+}
 `;
 
 const ActionGroup = styled.div`
@@ -59,12 +82,43 @@ const ActionGroup = styled.div`
   }
 `;
 
-const MainHeader = styled(MaxWidthWrapper)`
+const TabletDownLogo = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  @media (${QUERIES.laptopAndUp}) {
+    display: none;
+  }
+`;
+
+const LaptopUpLogo = styled(MaxWidthWrapper)`
+  display: none;
+  @media (${QUERIES.laptopAndUp}) {
+    display: revert;
+  }
+`;
+
+const TabletDownButton = styled.button`
+  @media (${QUERIES.laptopAndUp}) {
+    display: none;
+  }
+`;
+
+const LaptopUpSubscribe = styled.div`
+  display: none;
+  @media (${QUERIES.laptopAndUp}) {
+    display: revert;
+  }
+`;
+
+const SubscribeButton = styled.button`
+  background-color: var(--color-primary);
+  color: white;
+  border-radius: 4px;
+  padding: 11px 24px;
+  height: min-content;
 `;
 
 export default Header;
